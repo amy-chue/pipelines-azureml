74b0fb6ba78f1f80bfcfc7417f2ed360e87696f0
amy-chue <61218961+amy-chue@users.noreply.github.com>
2022-07-13 07:10:54 +0000

    commit

--- models/diabetes/train_old.py
+++ models/diabetes/train_old.py
@@ -27,18 +27,19 @@ data = {"train": {"X": X_train, "y": y_train},
 # list of numbers from 0.0 to 1.0 with a 0.05 interval
 alphas = np.arange(0.0, 1.0, 0.05)
 
+for alpha in alphas:
+    # Use Ridge algorithm to create a regression model
+    reg = Ridge(alpha=alpha)
+    reg.fit(data["train"]["X"], data["train"]["y"])
 
-reg = Ridge(alpha=0.95)
-reg.fit(data["train"]["X"], data["train"]["y"])
-
-preds = reg.predict(data["test"]["X"])
-mse = mean_squared_error(preds, data["test"]["y"])
-run.log('alpha', 0.95)
-run.log('mse', mse)
+    preds = reg.predict(data["test"]["X"])
+    mse = mean_squared_error(preds, data["test"]["y"])
+    run.log('alpha', alpha)
+    run.log('mse', mse)
 
     # Save model in the outputs folder so it automatically get uploaded when running on AML Compute
-model_file_name = 'ridge_0.95.pkl'.format(0.95)
-with open(model_file_name, "wb") as file:	
-    joblib.dump(value=reg, filename=os.path.join('./outputs/',model_file_name))
+    model_file_name = 'ridge_{0:.2f}.pkl'.format(alpha)
+    with open(model_file_name, "wb") as file:	
+        joblib.dump(value=reg, filename=os.path.join('./outputs/',model_file_name))
     
-print('alpha is {0:.2f}, and mse is {1:0.2f}'.format(0.95, mse))
+    print('alpha is {0:.2f}, and mse is {1:0.2f}'.format(alpha, mse))
