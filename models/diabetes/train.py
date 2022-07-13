0db4692b0a138e4a0145beaedeb6376be5abae4b
amy-chue <61218961+amy-chue@users.noreply.github.com>
2022-07-13 12:15:25 +1000

    Update train.py

--- models/diabetes/train.py
+++ models/diabetes/train.py
@@ -1,21 +1,21 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license.
 
+
+import os
+import numpy as np
 from sklearn.datasets import load_diabetes
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from azureml.core.run import Run
-import os
-import numpy as np
-# from sklearn import __version__ as sklearnver
-from packaging.version import Version
-    import joblib
-    
+
+from utils import mylib
+
 try:
-    from collections.abc import Iterator
+    from collections.abc import Iterable
 except ImportError:
-    from collections import Iterator
+    from collections import Iterable
 
 os.makedirs('./outputs', exist_ok=True)
 
@@ -30,7 +30,7 @@ data = {"train": {"X": X_train, "y": y_train},
         "test": {"X": X_test, "y": y_test}}
 
 # list of numbers from 0.0 to 1.0 with a 0.05 interval
-alphas = np.arange(0.0, 1.0, 0.05)
+alphas = mylib.get_alphas()
 
 for alpha in alphas:
     # Use Ridge algorithm to create a regression model
@@ -42,10 +42,7 @@ for alpha in alphas:
     run.log('alpha', alpha)
     run.log('mse', mse)
 
+    # Save model in the outputs folder so it automatically get uploaded when running on AML Compute
     model_file_name = 'ridge_{0:.2f}.pkl'.format(alpha)
-    # save model in the outputs folder so it automatically get uploaded
-    with open(model_file_name, "wb") as file:
-        joblib.dump(value=reg, filename=os.path.join('./outputs/',
-                                                     model_file_name))
-
-    print('alpha is {0:.2f}, and mse is {1:0.2f}'.format(alpha, mse))
+    with open(model_file_name, "wb") as file:	
+        joblib.dump(value=reg, filename=os.path.join('./outputs/',model_file_name))
